e825d69a36bb7765cfc0add962258333c4504ea0
Ivan Santos <pragmaticivan@gmail.com>
2013-11-12 12:02:17 -0600

    basic configs for grunt

---
+++ Gruntfile.js
@@ -0,0 +1,23 @@
+module.exports = function(grunt) {
+
+	grunt.initConfig({
+
+		// Import package manifest
+		pkg: grunt.file.readJSON("package.json"),
+
+		// Banner definitions
+		meta: {
+			banner: "/*\n" +
+				" *  <%= pkg.title || pkg.name %> - v<%= pkg.version %>\n" +
+				" *  <%= pkg.description %>\n" +
+				" *  <%= pkg.homepage %>\n" +
+				" *\n" +
+				" *  Made by <%= pkg.author.name %>\n" +
+				" *  Under <%= pkg.licenses[0].type %> License\n" +
+				" */\n"
+		},
+
+	});
+
+
+};
