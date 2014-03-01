bac300cdd446a1a172fc5e2c89379c9410827aa1
Ivan Santos <pragmaticivan@gmail.com>
2014-03-01 02:17:30 -0300

    build and dist files

---
+++ config/build.js
@@ -0,0 +1,12 @@
+module.exports = {
+  vendorFiles: [
+    'zepto/zepto.min.js',
+    'topcoat/css/topcoat-mobile-dark.min.css'
+  ],
+  htmlFiles: [
+    'index.html'
+  ],
+  cssFiles: [
+    'style.css'
+  ]
+};
