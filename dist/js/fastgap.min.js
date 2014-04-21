d8dd373e868da3ca5689337f90878adedc6b2c3b
Daniel Torres <danielfeelfine@gmail.com>
2014-04-21 03:42:19 -0300

    0.0.49

--- dist/js/fastgap.min.js
+++ dist/js/fastgap.min.js
@@ -1,5 +1,5 @@
 /*!
- * FastGap v0.0.48 (http://fastgap.mobi/)
+ * FastGap v0.0.49 (http://fastgap.mobi/)
  * Author: Gustavo Costa
  * Maintainers: https://github.com/orgs/FastGap/members
  * Copyright (c) 2014
